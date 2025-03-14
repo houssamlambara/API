4514aa560bdaeee2666132c9e0e3f95eb44bc17f
houssamlambara <lambarahoussam@gmail.com>
2025-03-14 14:59:18 +0000

    ajout des routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\itineraireController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('Auth', AuthController::class );
+Route::apiResource('Auth', AuthController::class);
+Route::post('Login', [AuthController::class, 'login']);
+Route::apiResource('itineraire', itineraireController::class)->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'store']);
